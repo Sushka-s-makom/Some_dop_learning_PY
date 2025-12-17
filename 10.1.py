2d7372f12c52d54d86cd693a5b576fd03a2beb89
Sushka-s-makom <zabrodskiyalex111206@gmail.com>
2025-12-17 22:25:31 +0300

    Мини конспекты по ввдению в словари

---
+++ 10.1.py
@@ -0,0 +1,6 @@
+languages_dic = { 'Artem' : ' 7 year old',
+              'Matvey' : ' 8 year old'}
+print(languages_dic['Artem'])
+'''с помощью фнкции задаем словарь'''
+
+info = dict( name = 'artem', age = '17', sex = 'trans')
---
+++ 10.2.py
@@ -0,0 +1,11 @@
+info = {'name': 'Sam',
+        'age': 28,
+        'job': 'Teacher'}
+
+info['name'] = 'Ben'
+print(info)
+
+d = {"a": 1, "b": 2}
+del d["a"]
+d.update( { "f" : 1, "c" : 6})
+print(d)
